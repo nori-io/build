f51b69c1cdfe08e4b8451eca582ef3e8407c77f6
Serge <serge@cheebo.me>
2020-12-16 10:39:45 +0500

    fix: fixed paths and namesm removed obsolet code

--- build/0.2.0/scripts/build.sh
+++ build/0.2.0/scripts/build.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
-cd /plugins
+cd /src
 
-if [[ "$build_mode" == "single" ]]; then
-echo "SINGLE"
 go mod download
-go build -buildmode=plugin -o /plugins/plugin.so /plugins/plugin.go
-fi
-
-
-if [[ "$build_mode" == "bulk" ]]; then
-echo "BULK"
-ls
-for directory in `find /plugins -mindepth 1 -maxdepth 1  -type d `
-do
-cd $directory
-echo $directory found
-go mod download
-go build -buildmode=plugin -o $directory.so plugin.go
-cd ..
-done
-fi
+go build -buildmode=plugin -o /src/plugin.so /src/plugin/main.go
